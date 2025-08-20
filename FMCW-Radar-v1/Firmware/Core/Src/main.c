b29227f03ff39c4c22814df11a3ad7fc288791fa
Sai Machiraju <smachiraju3@gatech.edu>
2025-08-20 07:13:10 -0400

    Basic ramp generation firmware.

--- FMCW-Radar-v1/Firmware/Core/Src/main.c
+++ FMCW-Radar-v1/Firmware/Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "stm32f3xx_hal_dac.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -57,6 +57,46 @@ static void MX_DAC_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+// This Function Provides Delay In Microseconds Using DWT
+// Source: https://deepbluembedded.com/stm32-delay-microsecond-millisecond-utility-dwt-delay-timer-delay/
+uint32_t DWT_Delay_Init(void)
+{
+    /* Disable TRC */
+    CoreDebug->DEMCR &= ~CoreDebug_DEMCR_TRCENA_Msk; // ~0x01000000;
+    /* Enable TRC */
+    CoreDebug->DEMCR |=  CoreDebug_DEMCR_TRCENA_Msk; // 0x01000000;
+
+    /* Disable clock cycle counter */
+    DWT->CTRL &= ~DWT_CTRL_CYCCNTENA_Msk; //~0x00000001;
+    /* Enable  clock cycle counter */
+    DWT->CTRL |=  DWT_CTRL_CYCCNTENA_Msk; //0x00000001;
+
+    /* Reset the clock cycle counter value */
+    DWT->CYCCNT = 0;
+
+    /* 3 NO OPERATION instructions */
+    __ASM volatile ("NOP");
+    __ASM volatile ("NOP");
+    __ASM volatile ("NOP");
+
+    /* Check if clock cycle counter has started */
+    if(DWT->CYCCNT)
+    {
+       return 0; /*clock cycle counter started*/
+    }
+    else
+    {
+      return 1; /*clock cycle counter not started*/
+    }
+}
+
+__STATIC_INLINE void DWT_Delay_us(volatile uint32_t au32_microseconds)
+{
+  uint32_t au32_initial_ticks = DWT->CYCCNT;
+  uint32_t au32_ticks = (HAL_RCC_GetHCLKFreq() / 1000000);
+  au32_microseconds *= au32_ticks;
+  while ((DWT->CYCCNT - au32_initial_ticks) < au32_microseconds-au32_ticks);
+}
 /* USER CODE END 0 */
 
 /**
@@ -89,16 +129,31 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_DAC_Init();
+  DWT_Delay_Init();
+
   /* USER CODE BEGIN 2 */
+  HAL_DAC_Start(&hdac, DAC_CHANNEL_1);
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  int startingAdcValue =(0.4 / 3.3) * 4096;
+  int endingAdcValue = (2.4 / 3.3) * 4096;
+
   while (1)
   {
     /* USER CODE END WHILE */
+	for (int i = startingAdcValue; i < endingAdcValue; i++) {
+		// STM32 is little-endian
+		// https://electronics.stackexchange.com/questions/565448/what-is-right-or-left-alignment-of-data-in-adc
+		if (HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_L, i << 4) != HAL_OK) {
+			Error_Handler();
+		}
 
+		// 5 ms ramp
+		DWT_Delay_us(5000 / (endingAdcValue - startingAdcValue));
+	}
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
